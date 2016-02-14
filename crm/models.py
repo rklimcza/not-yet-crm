a12119d9b2d41372ec7e38b5297980aa305f878a
Rafal Klimczak <rklimcza@gmail.com>
2016-02-14 21:07:00 +0100

    Added base of City and Contact mode

--- crm/models.py
+++ crm/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from datetime import date
 
 class Client(models.Model):
 	first_name = models.CharField(max_length = 20)
 	second_name = models.CharField(max_length = 20)
 	email = models.EmailField()
 	phone_number = models.CharField(max_length = 20)
+	comment = models.TextField()
+
+class City(models.Model):
+	name = models.CharField(max_length = 20)
+	
+class Contact(models.Model):
+	date = models.DateField(default=date.today)
 	
