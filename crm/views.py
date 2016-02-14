52a4be96aac73f566054fdc42badff6a547d05af
Rafal Klimczak <rklimcza@gmail.com>
2016-02-15 00:48:52 +0100

    Showing Clients by City

--- crm/views.py
+++ crm/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from crm.models import Client, City, Contact
 
 
-def client_list(request):
-	clients = Client.objects.all().order_by('city', 'first_name')
+def client_list(request, pk=0):
+	
+	pk = int(pk)
+	if pk == 0:
+		clients = Client.objects.all().order_by('city', 'first_name')
+	else:
+		clients = Client.objects.filter(city=pk).order_by('city', 'first_name')
+		
 	cities = City.objects.all().order_by('id')
 	
 	return render(request, 'crm/client_list.htm', {'clients': clients,
-												   'cities':cities, 
+												   'cities':cities,
+												   'city_now':pk,
 												  })
