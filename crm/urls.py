769c30af936007e8271f546966d47ace527dae2b
Rafal Klimczak <rklimcza@gmail.com>
2016-02-15 17:27:02 +0100

    changed addres for client_details view

--- crm/urls.py
+++ crm/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.client_list, name='client_list'),
     url(r'^(?P<pk>[0-9]+)/$', views.client_list, name='client_list'),
-    url(r'^client_details/(?P<pk>[0-9]+)$',
+    url(r'^client/(?P<pk>[0-9]+)$',
 		views.client_details, name='client_details'),
 	url(r'^logout/$', 'django.contrib.auth.views.logout',
                       {'next_page': '/'}),
